3e2e94277cbb967f80ac715119b3d809698b5e16
Ittai <ittai.pomson@mail.huji.ac.il>
2021-12-12 23:49:37 +0200

    added design to main screen

---
+++ app/src/main/java/com/moveo/notes/ListFrag.java
@@ -0,0 +1,36 @@
+package com.moveo.notes;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ListFrag extends Fragment {
+
+    private ListViewModel mViewModel;
+
+    public static ListFrag newInstance() {
+        return new ListFrag();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.list_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(ListViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
--- app/src/main/java/com/moveo/notes/MainScreen.java
+++ app/src/main/java/com/moveo/notes/MainScreen.java
@@ -1,14 +1,80 @@
 package com.moveo.notes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainScreen extends AppCompatActivity {
+    boolean MAP = true;
+    boolean LIST = false;
+    boolean lastPressed = MAP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MapFrag()).commit();
+
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            // By using switch we can easily get
+            // the selected fragment
+            // by using there id.
+            Fragment selectedFragment = null;
+            switch (item.getItemId()) {
+                case R.id.map:
+                    if(lastPressed == LIST) {
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .setCustomAnimations(R.anim.slide_in_left,
+                                        R.anim.slide_out_right,
+                                        R.anim.slide_in_right,
+                                        R.anim.slide_out_left)
+                                .replace(R.id.fragment_container, new MapFrag())
+                                .commit();
+                        lastPressed = MAP;
+                    }
+                    break;
+                case R.id.list:
+                    if(lastPressed == MAP) {
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .setCustomAnimations(
+                                        R.anim.slide_in_right,
+                                        R.anim.slide_out_left,
+                                        R.anim.slide_in_left,
+                                        R.anim.slide_out_right)
+                                .replace(R.id.fragment_container, new ListFrag())
+                                .commit();
+                        lastPressed = LIST;
+                    }
+                    break;
+
+            }
+            // It will help to replace the
+            // one fragment to other.
+//            getSupportFragmentManager()
+//                    .beginTransaction()
+//                    .replace(R.id.fragment_container, selectedFragment)
+//                    .commit();
+            return true;
+        }
+
+
+    };
 }

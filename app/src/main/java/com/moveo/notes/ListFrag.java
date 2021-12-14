1fcf84cd0e6eae290ab09d16b73553eb865835d1
Ittai <ittai.pomson@mail.huji.ac.il>
2021-12-14 18:35:00 +0200

    fixed recycler lagging problem. still not perfect

--- app/src/main/java/com/moveo/notes/ListFrag.java
+++ app/src/main/java/com/moveo/notes/ListFrag.java
@@ -1,44 +1,43 @@
 package com.moveo.notes;
 
 import androidx.databinding.DataBindingUtil;
-import androidx.databinding.ViewDataBinding;
-import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
-import android.content.res.TypedArray;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.ViewDataBinding;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
-import com.moveo.notes.databinding.ActivityMainBinding;
 import com.moveo.notes.databinding.ListFragmentBinding;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ListFrag extends Fragment {
     List<Note> notesList;
-    ListFragmentBinding bi;
+    View view;
     RecyclerAnimationAdapter adapter;
     NotesApp app;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+//
+//        bi = DataBindingUtil.inflate(
+//                inflater, R.layout.list_fragment, container, false);
+//        View view = bi.getRoot();
+//
+//        return view;
 
-        bi = DataBindingUtil.inflate(
-                inflater, R.layout.list_fragment, container, false);
-        View view = bi.getRoot();
-
+        view =  inflater.inflate(R.layout.list_fragment, container, false);
         return view;
+
 
     }
 
@@ -63,8 +62,9 @@ public class ListFrag extends Fragment {
                     getActivity().finish(); // todo: check if works
 
                 });
-        bi.myList.setLayoutManager(new LinearLayoutManager(getActivity()));
-        bi.myList.setAdapter(adapter);
+        androidx.recyclerview.widget.RecyclerView recyclerView = view.findViewById(R.id.myList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(adapter);
     }
 
 }
